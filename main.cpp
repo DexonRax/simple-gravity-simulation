700759180bd6210d9ac8639820d98d6062099960
Dexon Rax <dexonrax@gmail.com>
2025-02-18 18:01:56 +0100

    added freecam

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ const int VIEW_WIDTH = 16;
 const int VIEW_HEIGHT = 9;
 const double G = 6.67430e-11;
 const int PHYSICS_STEPS = 100;
-const int DRAW_SCALE = 1.0;
 
 struct Vector2 {
     double x, y;
@@ -89,10 +88,13 @@ Vector2 calculate_orbit_velocity(Planet &p1, Planet &p2){
 
 int main(){
 
-    double timeScale = 500.0;
+    double drawScale = 2.0;
+    double timeScale = 75.0;
     double initialViewScale = 5.0e5;
     double viewScale = initialViewScale;
     double deltaTime = 0.01;
+    double viewX = 0.0;
+    double viewY = 0.0;
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Gravity Simulation");
     sf::Clock deltaTimeClock;
@@ -113,7 +115,7 @@ int main(){
 		{1.0, 250.0, {0, 510000}, {0, 0}, {0, 0}, sf::Color::White} 
 
     };
-   
+
     planets[1].m_velocity = calculate_orbit_velocity(planets[1], planets[0]);
     planets[2].m_velocity = calculate_orbit_velocity(planets[2], planets[0]);
     planets[3].m_velocity = calculate_orbit_velocity(planets[3], planets[0]);
@@ -121,6 +123,31 @@ int main(){
 
     planets[5].m_velocity = calculate_orbit_velocity(planets[5], planets[0]);
     planets[6].m_velocity = calculate_orbit_velocity(planets[6], planets[0]) + calculate_orbit_velocity(planets[6], planets[5]);
+
+
+   /*
+    const int planetCount = 8;
+    Planet planets[planetCount] = {
+        {1.989e30, 696340, {0, 0}, {0, 0}, {0, 0}, sf::Color::Yellow}, // Sun
+        {3.301e23, 2439.7, {57900000, 0}, {0, 47.87}, {0, 0}, sf::Color(169, 169, 169)}, // Mercury
+        {4.867e24, 6051.8, {108200000, 0}, {0, 35.02}, {0, 0}, sf::Color(255, 165, 0)}, // Venus
+        {5.972e24, 6371, {149600000, 0}, {0, 29.78}, {0, 0}, sf::Color::Blue}, // Earth
+        {6.417e23, 3389.5, {227900000, 0}, {0, 24.07}, {0, 0}, sf::Color::Red}, // Mars
+        {1.898e27, 69911, {778500000, 0}, {0, 13.07}, {0, 0}, sf::Color(218, 165, 32)}, // Jupiter
+        {5.683e26, 58232, {1429000000, 0}, {0, 9.69}, {0, 0}, sf::Color(210, 180, 140)}, // Saturn
+        {8.681e25, 25362, {2871000000, 0}, {0, 6.81}, {0, 0}, sf::Color(173, 216, 230)}  // Uranus
+    };
+
+    planets[1].m_velocity = calculate_orbit_velocity(planets[1], planets[0]);
+    planets[2].m_velocity = calculate_orbit_velocity(planets[2], planets[0]);
+    planets[3].m_velocity = calculate_orbit_velocity(planets[3], planets[0]);
+    planets[4].m_velocity = calculate_orbit_velocity(planets[4], planets[0]);
+    planets[5].m_velocity = calculate_orbit_velocity(planets[5], planets[0]);
+    planets[6].m_velocity = calculate_orbit_velocity(planets[6], planets[0]);
+    planets[7].m_velocity = calculate_orbit_velocity(planets[7], planets[0]);
+    */
+
+
 
     std::cout<<"initialization finalized\n";
 
@@ -149,9 +176,35 @@ int main(){
             view.setSize(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
             view.setCenter(planets[viewPlanet].m_position.x, planets[viewPlanet].m_position.y); 
         }else{
-            viewScale = initialViewScale;
+            //viewScale = initialViewScale;
+            //view.setSize(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
+            float velX = 0.0f, velY = 0.0f;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                velX -= 1.f;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                velX += 1.f;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                velY -= 1.f;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                velY += 1.f;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::F1))
+                viewScale += viewScale * 0.002;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::F2))
+                viewScale -= viewScale * 0.002;
+
+            std::cout<<"view scale: "<<viewScale<<"\n";
+
+            float length = sqrt(pow(velX, 2) + pow(velY, 2));
+            if (length != 0) {
+                velX = velX / length;
+                velY = velY / length;
+            }
+
+            viewX += velX * viewScale * deltaTime;
+            viewY += velY * viewScale * deltaTime;
+
+            view.setCenter(sf::Vector2f(round(viewX), round(viewY)));
             view.setSize(VIEW_WIDTH * viewScale, VIEW_HEIGHT * viewScale);
-            view.setCenter(0, 0);
         }
 
         window.clear();
@@ -162,8 +215,8 @@ int main(){
         
 
         for (int i = 0; i < planetCount; i++){
-            planetDrawable.setRadius(planets[i].m_radius * DRAW_SCALE);
-            planetDrawable.setOrigin(planets[i].m_radius * DRAW_SCALE, planets[i].m_radius * DRAW_SCALE);
+            planetDrawable.setRadius(planets[i].m_radius * drawScale);
+            planetDrawable.setOrigin(planets[i].m_radius * drawScale, planets[i].m_radius * drawScale);
             planetDrawable.setPosition(sf::Vector2f(planets[i].m_position.x, planets[i].m_position.y));
             planetDrawable.setFillColor(planets[i].m_color);
             window.draw(planetDrawable);
